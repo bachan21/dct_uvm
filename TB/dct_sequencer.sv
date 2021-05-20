3513be4fafcecd62bfb9dcabf79b8402956249a5
bachan21 <bachanshyam999@gmail.com>
2021-05-20 21:33:21 +0530

    Delete TB directory

--- TB/dct_sequencer.sv
+++
@@ -1,9 +0,0 @@
-
- class dct_sequencer extends uvm_sequencer#(dct_sequence_item);
-	`uvm_component_utils(dct_sequencer)
-    extern function new(string name="DCT_SEQUENCER",uvm_component parent);
- endclass : dct_sequencer
- 
- function dct_sequencer:: new(string name="DCT_SEQUENCER",uvm_component parent);
-	super.new(name,parent);
- endfunction : new
